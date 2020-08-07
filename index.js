ee13fb5b2c78d1069c309efc3ae391b3fb6ddb31
guilherme terriaga <guilherme.terriaga@gmail.com>
2020-08-07 15:44:58 -0300

    created ppost function

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ server.get("/users/:index", (req, res) => {
   return res.json(users[index]);
 });
 
-server.post("/users", (req, res) => {});
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
 
 server.listen(3000);
 

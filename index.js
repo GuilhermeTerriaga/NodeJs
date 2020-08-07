c38c5292a6bfafb3f0ea40dc15095db3a5f2a13c
guilherme terriaga <guilherme.terriaga@gmail.com>
2020-08-07 16:17:43 -0300

    CRUD complete with array as DB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
+
 const users = ["usuarioA", "usuarioB", "usuarioC", "usuarioD"];
 
 server.get("/users", (req, res) => {
@@ -16,6 +18,20 @@ server.post("/users", (req, res) => {
   const { name } = req.body;
   users.push(name);
   return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  return res.send();
 });
 
 server.listen(3000);
